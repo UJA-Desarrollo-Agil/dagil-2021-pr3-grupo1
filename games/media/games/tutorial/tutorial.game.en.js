eae85f031ddd79ede70bbe80f6295df4aa7f66f8
Cristian <chl00014@red.ujaen.es>
2021-03-30 21:32:57 +0200

    Realizacion de situaciones capitulo 3\, falta eleccion de armas

--- games/media/games/tutorial/tutorial.game.en.js
+++ games/media/games/tutorial/tutorial.game.en.js
@@ -29,23 +29,121 @@ undum.game.slideUpSpeed = 500
 /* The situations that the game can be in. Each has a unique ID. */
 undum.game.situations = {
     start: new undum.SimpleSituation(
-        "<h1>Starting Out with Undum</h1>\
-        <img src='media/games/tutorial/woodcut1.png' class='float_right'>\
-        <p>Welcome to the Undum tutorial. Undum is a tool for writing\
-        hypertext interactive fiction. It has some unique features\
-        and a visual design that encourages narrative games.</p>\
+        "<h1>Capítulo 3 - MI OBJETIVO</h1>\
+        <p>Tras conocer la ubicación <br> de mi ordenador y preguntar por las rutas existentes me planteo cuál seguir.</p>\
         \
-        <p>Hypertext interactive fiction is the digital equivalent of the\
-        Choose Your Own Adventure (CYOA) books that were popular in the\
-        1980s. The story is told in chunks, and you select from a range\
-        of options to move it forward. Unlike the book form, however, the\
-        digital form gives you far more flexibility to tell rich stories\
-        and introduce more interesting game elements.</p>\
+        <p>Por la ruta más larga tengo un <a href='campamento'>campamento de villanos</a>, según los datos proporcionados por el gnomo habitan cinco villanos y van armados con espadas de metal y escudos de madera.</p>\
         \
-        <p class='transient'>Click <a href='hub'>this link to\
-        continue...</a></p>"
+        <p>Por la ruta más corta tengo que <a href='montana'>pasar la montaña</a>, gobernada por un monstruo del cual no se conoce información, solo sé que es conocido como \"El rey de la montaña\".</p>"
     ),
 
+    /*Elección campamento de villanos*/
+    campamento: new undum.SimpleSituation(
+        "<p>Decido ir por la ruta más larga, <br> pero de la que más información tengo, llegar hasta allí me llevará cinco días y cuatro noches, por lo que decido partir ya.</p>\
+        <p>Tras varias días caminando sin cesar, diviso a lo lejos el campamento de villanos. Para mi asombro solo está compuesto por cabañas de madera con el techo de paja y de un río que cruza por el centro.</p>\
+        <p><a href='detalladamente_campamento'>Mirar detalladamente el campamento</a>.</p>\
+        <p>Permanezco escondido y comienzo a preparar el plan para atacar, pudiendo hacerlo de dos formas distintas, o bien <a href='campamento_fuerza'>entro a la fuerza</a> atacando e intentando pillarlos desprevenidos o me centro en <a href='campamento_sigilo'>hacerlo sigilosamente</a>.</p>",
+        {
+            heading: "Campamento de villanos",
+        }
+    ),
+    campamento2: new undum.SimpleSituation(
+        "<p>Decido ir por la ruta más larga, <br> pero de la que más información tengo, llegar hasta allí me llevará cinco días y cuatro noches, por lo que decido partir ya.</p>\
+        <p>Tras varias días caminando sin cesar, diviso a lo lejos el campamento de villanos. Para mi asombro solo está compuesto por cabañas de madera con el techo de paja y de un río que cruza por el centro.</p>\
+        <p>Permanezco escondido y comienzo a preparar el plan para atacar, pudiendo hacerlo de dos formas distintas, o bien <a href='campamento_fuerza'>entro a la fuerza</a> atacando e intentando pillarlos desprevenidos o me centro en <a href='campamento_sigilo'>hacerlo sigilosamente</a>.</p>",
+        {
+            heading: "Campamento de villanos",
+        }
+    ),
+    /*Mirar detalladamente el campamento*/
+    detalladamente_campamento: new undum.SimpleSituation(
+        "<p>Tras mirar detallademente, veo que hay un tirador con arco y flechas de piedra.</p>\
+        <p><a href='campamento2'>Continuar con la historia</a></p>",
+        {
+            heading: "Mirar detalladamente el campamento",
+        }
+    ),
+    /*Elección campamento de villanos a la fuerza*/
+    campamento_fuerza: new undum.SimpleSituation(
+        "<p>Decido hacer el ataque por la fuerza, <br> con mi espada en mano salgo a correr hacia el campamento entrando por la parte trasera y acabando con la vida de dos villanos. En este momento los dos que se encuentran en tierra más el arquero subido en una plataforma se focalizan en mí. Esquivando flechas y luchando contra los dos a la vez consigo salir victorioso pero con heridas físicas graves, por lo que tendré que recuperarme antes de poder seguir.</p>\
+        <p>Me tumbo en una de las camas del campamento para <a href='campamento_fuerza2'> reposar y sanar mis heridas</a>.</p>",
+        {
+            heading: "Ataque al campamento por la fuerza",
+        }
+    ),
+    campamento_fuerza2: new undum.SimpleSituation(
+        "<p> Tras una semana, <br> mis heridas han sanado y procedo a prepararme para llegar al castillo del jefe, quien ha robado mi ordenador.</p>\
+        <p>Antes de salir voy a llevar conmigo algunas de las <a href='eleccion_villanos'>armas que tenían los villanos</a></p>",
+        {
+            heading: "Recuperación",
+        }
+    ),
+    /*Elección campamento de villanos en sigilo*/
+    campamento_sigilo: new undum.SimpleSituation(
+        "<p>Decido hacer un ataque sigiloso, <br> con mi espada en mano entro de noche en el campamento nadando por el río.</p>\
+        <p>Mi mayor prioridad es acabar con el arquero, ya que es el que presenta la gran amenaza. Me avalanzo sobre él por la espalda sin que el resto se percate. Cojo su arco y acabo con dos soldados que se encontraban de guardia, bajo y acaba con la vida de los soldados que estaban descansando en sus cabañas.</p>\
+        <p>Decido pasar la noche en el campamento y antes de partir hacia el castillo <a href='eleccion_villanos'>recojo algunas armas pertenecientes a los villanos.</a></p>",
+        {
+            heading: "Ataque al campamento sigilosamente",
+        }
+    ),
+    
+    /*Elección armas de villanos*/
+    eleccion_villanos: new undum.SimpleSituation(
+        "<p>.</p>",
+        {
+            heading: "Elección de armas",
+        }
+    ),
+    
+    /*Elección pasar por la montaña*/
+    montana: new undum.SimpleSituation(
+        "<p>Decido ir por la ruta más corta, <br>pese a no tener información sobre el monstruo.</p>\
+        <p>Tras un dia de camino llego a las llanuras de la montaña, noto algo inquietante y es que la vegetación y el clima son muy secos.</p>\
+        <p>Me adentro en ella y continuo recto durante el día. Al llegar la noche decido continuar sin parar hasta que noto como algo me vigila en la distancia, decido correr y algo se avalanza sobre mí, consigo escaparme y verlo de frente, se trata de un lobo enorme.</p>\
+        <p>Tengo poco tiempo para pensar ya que se volvera a avalanzar sobre mi, nose si debería <a href='salir_corriendo'>salir corriendo</a> o <a href='enfrentarme'>enfrentarme a él</a>.</p>",
+        {
+            heading: "El rey de la montaña",
+        }
+    ),
+    
+    /*Salir corriendo del lobo*/
+    salir_corriendo: new undum.SimpleSituation(
+        "<p>Tras pensarlo rapidamente, <br>decido salir corriendo entre la maleza del bosque, el lobo me persigue sin cesar por lo que no me queda otra que darme la vuelta y <a href='enfrentarme'>enfrentarme a él</p>",
+        {
+            heading: "Salir corriendo",
+        }
+    ),
+    
+    /*Enfrentarme a el*/
+    enfrentarme: new undum.SimpleSituation(
+        "<p>Cojo mi espada, <br> y salgo corriendo hacia él, me muerde el brazo mientras le atravieso el cuello con mi arma.\
+        El resultado ha sido desastroso, mi brazo ha acabado destrozado, pero al menos he conseguido sobrevivir ante semejante amenaza.</p>\
+        <p>Tras varios días de recuperación, el gnomo me cuenta que se trata de un animal mágico y que sus huesos sirven para hacer armas mas fuertes que el hierro.</p>\
+        <p>Puesto que no tengo todo el tiempo del mundo debo decidir que <a href='eleccion_montana'>armas fabricar</a>.</p>",
+        {
+            heading: "Enfrentamiento con el lobo",
+        }
+    ),
+    /*Elección armas montaña*/
+    eleccion_montana: new undum.SimpleSituation(
+        "<p>.</p>",
+        {
+            heading: "Elección de armas",
+        }
+    ),
+    
+    /*llegada a castillo*/
+    llegada_castillo: new undum.SimpleSituation(
+        "<p>Tras reponer mi inventario con nuevas armas, <br> focalizo mi atención en llegar hacia el castillo.</p>\
+        <p>Tras un día caminando sin descanso diviso a lo lejos el inmenso castillo del responsable de robar mi ordenador.</p>\
+        <p><a href='cap4'>Me dirigo hacia la puerta principal</a></p>",
+        {
+            heading: "Llegada al castillo",
+        }
+    ),
+    
+    
     // NB: The 'hub' situation which is the main list of topics, is
     // defined wholly in the HTML file, and doesn't have an entry in
     // the game.situations dictionary in this file.
