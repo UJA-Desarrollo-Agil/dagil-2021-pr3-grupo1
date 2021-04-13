8364f8db9b6c027586b92284b0e7b0badafb17d2
Cristian <chl00014@red.ujaen.es>
2021-04-13 12:16:41 +0200

    Imagenes en parte cristian

--- games/media/games/codigo.js
+++ games/media/games/codigo.js
@@ -271,7 +271,8 @@ undum.game.situations = {
 
   /*Comienzo capitulo 3*/
   capitulo_tercero: new undum.SimpleSituation(
-    "<p>Tras conocer la ubicación <br> de mi ordenador y preguntar por las rutas existentes me planteo cuál seguir.</p>\
+    "<img src='./media/img/14.jpg' class='float_right' width='500' height='250'>\
+    <p>Tras conocer la ubicación <br> de mi ordenador y preguntar por las rutas existentes me planteo cuál seguir.</p>\
         <p>Por la ruta más larga tengo un <a href='campamento'>campamento de villanos</a>, según los datos proporcionados por el gnomo habitan cinco villanos y van armados con espadas de metal y escudos de madera.</p>\
         <p>Por la ruta más corta tengo que <a href='montana'>pasar la montaña</a>, gobernada por un monstruo del cual no se conoce información, solo sé que es conocido como \"El rey de la montaña\".</p>",
     {
@@ -281,7 +282,8 @@ undum.game.situations = {
 
   /*Elección campamento de villanos*/
   campamento: new undum.SimpleSituation(
-    "<p>Decido ir por la ruta más larga, <br> pero de la que más información tengo, llegar hasta allí me llevará cinco días y cuatro noches, por lo que decido partir ya.</p>\
+    "<img src='./media/img/25.jpg' class='float_right' width='500' height='300'>\
+    <p>Decido ir por la ruta más larga, <br> pero de la que más información tengo, llegar hasta allí me llevará cinco días y cuatro noches, por lo que decido partir ya.</p>\
         <p>Tras varias días caminando sin cesar, diviso a lo lejos el campamento de villanos. Para mi asombro solo está compuesto por cabañas de madera con el techo de paja y de un río que cruza por el centro.</p>\
         <p><a href='detalladamente_campamento'>Mirar detalladamente el campamento</a>.</p>\
         <p>Permanezco escondido y comienzo a preparar el plan para atacar, pudiendo hacerlo de dos formas distintas, o bien <a href='campamento_fuerza'>entro a la fuerza</a> atacando e intentando pillarlos desprevenidos o me centro en <a href='campamento_sigilo'>hacerlo sigilosamente</a>.</p>",
@@ -296,7 +298,8 @@ undum.game.situations = {
     }
   ),
   campamento2: new undum.SimpleSituation(
-    "<p>Decido ir por la ruta más larga, <br> pero de la que más información tengo, llegar hasta allí me llevará cinco días y cuatro noches, por lo que decido partir ya.</p>\
+    "<img src='./media/img/25.jpg' class='float_right' width='500' height='300'>\
+    <p>Decido ir por la ruta más larga, <br> pero de la que más información tengo, llegar hasta allí me llevará cinco días y cuatro noches, por lo que decido partir ya.</p>\
         <p>Tras varias días caminando sin cesar, diviso a lo lejos el campamento de villanos. Para mi asombro solo está compuesto por cabañas de madera con el techo de paja y de un río que cruza por el centro.</p>\
         <p>Permanezco escondido y comienzo a preparar el plan para atacar, pudiendo hacerlo de dos formas distintas, o bien <a href='campamento_fuerza'>entro a la fuerza</a> atacando e intentando pillarlos desprevenidos o me centro en <a href='campamento_sigilo'>hacerlo sigilosamente</a>.</p>",
     {
@@ -319,7 +322,8 @@ undum.game.situations = {
   ),
   /*Elección campamento de villanos a la fuerza*/
   campamento_fuerza: new undum.SimpleSituation(
-    "<p>Decido hacer el ataque por la fuerza, <br> con mi espada en mano salgo a correr hacia el campamento entrando por la parte trasera y acabando con la vida de dos villanos. En este momento los dos que se encuentran en tierra más el arquero subido en una plataforma se focalizan en mí. Esquivando flechas y luchando contra los dos a la vez consigo salir victorioso pero con heridas físicas graves, por lo que tendré que recuperarme antes de poder seguir.</p>\
+    "<img src='./media/img/16.png' class='float_right' width='250' height='250'>\
+    <p>Decido hacer el ataque por la fuerza, <br> con mi espada en mano salgo a correr hacia el campamento entrando por la parte trasera y acabando con la vida de dos villanos. En este momento los dos que se encuentran en tierra más el arquero subido en una plataforma se focalizan en mí. Esquivando flechas y luchando contra los dos a la vez consigo salir victorioso pero con heridas físicas graves, por lo que tendré que recuperarme antes de poder seguir.</p>\
         <p>Me tumbo en una de las camas del campamento para <a href='campamento_fuerza2'> reposar y sanar mis heridas</a>.</p>",
     {
       heading: "Ataque al campamento por la fuerza",
@@ -335,7 +339,8 @@ undum.game.situations = {
     enter: function (character, system, from) {
       if (character.qualities.frascoVida > 0) {
         system.write(
-          "<p>Al tumbarme sobre la cama de una cabaña, recuerdo el frasco de vida que me dio Momo.</p>\
+          "<img src='./media/img/19.png' class='float_right' width='250' height='300'>\
+          <p>Al tumbarme sobre la cama de una cabaña, recuerdo el frasco de vida que me dio Momo.</p>\
                     <p>Decido tomarmelo de un trago y, para mi asombro todas mis heridas han sanado en cueston de segundos.\
                     Gracias a esto podré <a href='eleccion_villanos'>continuar mi viaje</a> sin perder tiempo.</p>"
         );
@@ -343,7 +348,8 @@ undum.game.situations = {
         system.setQuality("frascoVida", character.qualities.frascoVida - 1);
       } else {
         system.write(
-          "<p> Tras una semana, <br> mis heridas han sanado y procedo a prepararme para llegar al castillo del jefe,</p>\
+          "<img src='./media/img/12.png' class='float_right' width='150' height='150'>\
+          <p> Tras una semana, <br> mis heridas han sanado y procedo a prepararme para llegar al castillo del jefe,</p>\
           quien ha robado mi ordenador.</p>\
           <p>Antes de salir voy a llevar conmigo algunas de las\
           <a href='eleccion_villanos'>armas que tenían los villanos</a></p>"
@@ -355,7 +361,8 @@ undum.game.situations = {
   }),
   /*Elección campamento de villanos en sigilo*/
   campamento_sigilo: new undum.SimpleSituation(
-    "<p>Decido hacer un ataque sigiloso, <br> con mi espada en mano entro de noche en el campamento nadando por el río.</p>\
+    "<img src='./media/img/2.png' class='float_right' width='250' height='250'>\
+    <p>Decido hacer un ataque sigiloso, <br> con mi espada en mano entro de noche en el campamento nadando por el río.</p>\
         <p>Mi mayor prioridad es acabar con el arquero, ya que es el que presenta la gran amenaza. Me avalanzo sobre él por la espalda sin que el resto se percate. Cojo su arco y acabo con dos soldados que se encontraban de guardia, bajo y acaba con la vida de los soldados que estaban descansando en sus cabañas.</p>\
         <p>Decido pasar la noche en el campamento y antes de partir hacia el castillo <a href='eleccion_villanos'>recojo algunas armas pertenecientes a los villanos.</a></p>",
     {
@@ -370,7 +377,8 @@ undum.game.situations = {
   ),
   /*Elección pasar por la montaña*/
   montana: new undum.SimpleSituation(
-    "<p>Decido ir por la ruta más corta, <br>pese a no tener información sobre el monstruo.</p>\
+    "<img src='./media/img/5.png' class='float_right' width='250' height='250'>\
+    <p>Decido ir por la ruta más corta, <br>pese a no tener información sobre el monstruo.</p>\
         <p>Tras un dia de camino llego a las llanuras de la montaña, noto algo inquietante y es que la vegetación y el clima son muy secos.</p>\
         <p>Me adentro en ella y continuo recto durante el día. Al llegar la noche decido continuar sin parar hasta que noto como algo me vigila en la distancia, decido correr y algo se avalanza sobre mí, consigo escaparme y verlo de frente, se trata de un lobo enorme.</p>\
         <p>Tengo poco tiempo para pensar ya que se volvera a avalanzar sobre mi, nose si debería <a href='salir_corriendo'>salir corriendo</a> o <a href='enfrentarme'>enfrentarme a él</a>.</p>",
@@ -386,7 +394,8 @@ undum.game.situations = {
   ),
   /*Salir corriendo del lobo*/
   salir_corriendo: new undum.SimpleSituation(
-    "<p>Tras pensarlo rapidamente, <br>decido salir corriendo entre la maleza del bosque, el lobo me persigue sin cesar por lo que no me queda otra que darme la vuelta y <a href='enfrentarme'>enfrentarme a él</p>",
+    "<img src='./media/img/24.jpg' class='float_right' width='500' height='300'>\
+    <p>Tras pensarlo rapidamente, <br>decido salir corriendo entre la maleza del bosque, el lobo me persigue sin cesar por lo que no me queda otra que darme la vuelta y <a href='enfrentarme'>enfrentarme a él</p>",
     {
       heading: "Salir corriendo",
     }
@@ -396,7 +405,8 @@ undum.game.situations = {
     enter: function (character, system, from) {
       if (character.qualities.frascoVida > 0) {
         system.write(
-          "<p>Cojo mi espada, <br> y salgo corriendo hacia él, me muerde el brazo mientras le atravieso el cuello con mi arma.\
+          "<img src='./media/img/4.png' class='float_right' width='250' height='250'>\
+          <p>Cojo mi espada, <br> y salgo corriendo hacia él, me muerde el brazo mientras le atravieso el cuello con mi arma.\
                     El resultado ha sido desastroso, mi brazo ha acabado destrozado, pero al menos he conseguido sobrevivir ante semejante amenaza.</p>\
                     <p>Tras acabar la batalla me tumbo en el suelo del bosque destrozado y recuerdo el frasco de vida que me dió Momo,\
                     decido tomarlo de un trago y, para mi asombro ¡Qué está pasando! en cuestión de segundos mis heridas han sanado y mi brazo\
@@ -411,7 +421,8 @@ undum.game.situations = {
         );
       } else {
         system.write(
-          "<p>Cojo mi espada, <br> y salgo corriendo hacia él, me muerde el brazo mientras le atravieso el cuello con mi arma.\
+          "<img src='./media/img/4.png' class='float_right' width='250' height='250'>\
+          <p>Cojo mi espada, <br> y salgo corriendo hacia él, me muerde el brazo mientras le atravieso el cuello con mi arma.\
                     El resultado ha sido desastroso, mi brazo ha acabado destrozado, pero al menos he conseguido sobrevivir ante semejante amenaza.</p>\
                     <p>Tras varios días de recuperación, el gnomo me cuenta que se trata de un animal mágico y que sus huesos sirven para hacer armas mas fuertes que el hierro.</p>\
                     <p>Puesto que no tengo todo el tiempo del mundo debo decidir que <a href='eleccion_montana'>armas fabricar</a>.</p>"
@@ -427,7 +438,8 @@ undum.game.situations = {
   }),
   /*llegada a castillo*/
   llegada_castillo: new undum.SimpleSituation(
-    "<p>Tras reponer mi inventario con nuevas armas, <br> focalizo mi atención en llegar hacia el castillo.</p>\
+    "<img src='./media/img/28.png' class='float_right' width='250' height='250'>\
+    <p>Tras reponer mi inventario con nuevas armas, <br> focalizo mi atención en llegar hacia el castillo.</p>\
         <p>Tras un día caminando sin descanso diviso a lo lejos el inmenso castillo del responsable de robar mi ordenador.</p>\
         <p><a href='entrada'>Me dirigo hacia la puerta principal</a></p>",
     {
